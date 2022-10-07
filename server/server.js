4896ab0f1a28a66a2542fd8fc4bd44053734fb73
reduansj <reduansenoun@gmail.com>
2022-10-07 12:17:01 +0200

    feat(dependencies): add server dependencies

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-console.log("hola matias");
+//!DEPENDENCIES
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+//!ADD ENVIRONMENT VARIABLES
+require("dotenv").config();
+
+//!PORT TO LISTEN
+app.listen(process.env.PORT, () => {
+  console.log(`App express is running in port: ${process.env.PORT}`);
+});
