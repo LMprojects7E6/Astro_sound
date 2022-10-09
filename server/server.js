b6a22ce5a2d9808010d22601da6a4dd262763d49
Ivan Escribano <ivanescribano1998@gmail.com>
2022-10-09 02:01:47 +0200

    feat(server and add dependencies): added some configuration,dependencies, middleware, connection database

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+//!ADD ENVIRONMENT VARIABLES
+require("dotenv").config();
+
 //!DEPENDENCIES
 const express = require("express");
 const app = express();
@@ -5,8 +8,22 @@ const helmet = require("helmet");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-//!ADD ENVIRONMENT VARIABLES
-require("dotenv").config();
+//!CONNECTION DB
+const connect = require("./config/db");
+connect();
+
+//!MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+const corsOptions = {
+  //Allow to set cookies in header
+  credentials: true,
+  //allow petitions from this domain when deploying maybe change to actual domain name
+  origin: process.env.CLIENT_URL,
+};
+app.use(cors(corsOptions));
+app.use(cookieParser());
 
 //!PORT TO LISTEN
 app.listen(process.env.PORT, () => {
