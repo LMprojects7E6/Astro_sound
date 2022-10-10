f1804e1a7ab4cc291ab4161179f7c2f19ee04ebe
MiguelD-PC <mikdominguez@gmail.com>
2022-10-10 16:42:05 +0100

    nomenclature changes

--- server/routes/songsRoutes.js
+++ server/routes/songsRoutes.js
@@ -1,9 +1,14 @@
+//!IMPORT DEPENDENCIES
 const express = require("express");
 const router = express.Router();
-const songsController = require("../controllers/songsController");
+const songsController =  require("../controllers/songs-controller.js");
 
-//CRUD
-
-router.get("/", songsController.getThreeSongs);
+//!CRUD
+router.get("/", songsController.getAllSongs());
+router.get("/", songsController.getThreeSongs());
+router.get("/:genre", songsController.getSongsByGenre());
+router.get("/:playlistID", songsController.getAllSongsFromPlaylist());
+router.post("/:playlistID", songsController.addSongToPlaylist());
+router.delete("/:SongID", songsController.removeSongFromPlaylist());
 
 module.exports = router;
