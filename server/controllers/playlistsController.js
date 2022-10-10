585bcd4787e361fbbb596dc50fa39454d98ac9a5
MiguelD-PC <mikdominguez@gmail.com>
2022-10-10 17:20:00 +0100

    code correction - endpoints

--- server/controllers/playlistsController.js
+++ server/controllers/playlistsController.js
@@ -4,38 +4,21 @@ const model = require("../models");
 //!GET PLAYLISTS
 const getAllPlaylists = async (req, res) => {
 
-    try {
-        res.status(200).send("ok");
-    } catch (error) {
-        
-    }
 };
 
 //!GET PLAYLISTS WITH ID
 const getPlaylistsByID = async (req, res) => {
-    try {
-        res.status(200).send("ok");
-    } catch (error) {
-        
-    }
+    
 };
 
 //!POST NEW PLAYLIST
 const createPlaylist = async (req, res) => {
-    try {
-        res.status(200).send("ok");
-    } catch (error) {
-        
-    }
+
 }
 
 //!PUT UPDATE PLAYLIST WITH ID
 const updatePlaylist = async (req, res) => {
-    try {
-        res.status(200).send("ok");
-    } catch (error) {
-        
-    }
+
 }
 
 module.exports = {
