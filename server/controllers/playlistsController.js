06edb6f70df3fb786e652c9f01fd2e67c1d93f7d
MiguelD-PC <mikdominguez@gmail.com>
2022-10-11 11:48:23 +0100

    getAllPlaylists and getLikedPlaylists development

--- server/controllers/playlistsController.js
+++ server/controllers/playlistsController.js
@@ -3,13 +3,50 @@ const model = require("../models");
 
 //!GET ALL USER PLAYLISTS
 const getAllPlaylists = async (req, res) => {
-  const { userID } = req.id;
+  // const { userID } = req.id;
 
   try {
-    const playlistsArray = await model.User.findById(userID).populate(
-      "playlists"
-    );
+    // const playlistsArray = await model.User.findById(userID).populate("playlists");
+    //!CODE USED FOR TESTING
+    const playlistsArray = await model.User.findById("6342151708c75ff62ab26e38").populate("playlists");
 
+    //GET PLAYLISTS OBJECTS
+    const { playlists } = playlistsArray;
+    res.status(200).send(playlists);
+  } catch (error) {
+    res.status(504).send({ message: error.message });
+  }
+};
+
+//!GET THE USERS LIKED PLAYLIST
+const getLikedPlaylists = async (req, res) => {
+  // const { userID } = req.id;
+
+  try {
+    //Get 5 playlists from the user that isn't the favorite playlist
+    // const playlistsArray = await model.User.findById(userID).populate("playlists",null, {"name": "favorite"});
+    //!CODE USED FOR TESTING
+    const playlistsArray = await model.User.findById("6342151708c75ff62ab26e38").populate("playlists",null, {"name": "favorite"});
+
+    //GET PLAYLISTS OBJECTS
+    const { playlists } = playlistsArray;
+    res.status(200).send(playlists);
+  } catch (error) {
+    res.status(504).send({ message: error.message });
+  }
+};
+
+//!GET 5 PLAYLISTS FROM USER
+const getFivePlaylists = async (req, res) => {
+  // const { userID } = req.id;
+
+  try {
+    //Get 5 playlists from the user that isn't the favorite playlist
+    const playlistsArray = await model.User.findById(userID).populate("playlists",null, {"name": {$ne: "favorite"}}, {limit: 5});
+    //!CODE USED FOR TESTING
+    // const playlistsArray = await model.User.findById("6342151708c75ff62ab26e38").populate("playlists",null, {"name": {$ne: "favorite"}}, {limit: 5});
+
+    //GET PLAYLISTS OBJECTS
     const { playlists } = playlistsArray;
     res.status(200).send(playlists);
   } catch (error) {
@@ -30,13 +67,17 @@ const getPlaylistsByID = async (req, res) => {
 };
 
 //!POST NEW PLAYLIST
-const createPlaylist = async (req, res) => {};
+const createPlaylist = async (req, res) => {
+  const {} = req.body;
+};
 
 //!PUT UPDATE PLAYLIST WITH ID
 const updatePlaylist = async (req, res) => {};
 
 module.exports = {
   getAllPlaylists: getAllPlaylists,
+  getLikedPlaylists: getLikedPlaylists,
+  getFivePlaylists: getFivePlaylists,
   getPlaylistsByID: getPlaylistsByID,
   createPlaylist: createPlaylist,
   updatePlaylist: updatePlaylist,
