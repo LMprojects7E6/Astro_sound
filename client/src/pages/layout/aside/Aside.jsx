1efa1a0a456891aee4580677b2aae0130ef11056
MatiasPatrignani <m.patrignani56@gmail.com>
2022-10-11 12:01:28 +0200

    Create Branch mMusic-Player

--- client/src/pages/layout/aside/Aside.jsx
+++ client/src/pages/layout/aside/Aside.jsx
@@ -6,7 +6,7 @@ import  Icon  from '../../../components/icons/Icons';
 const Aside = () => {
     const style = "h-10 flex gap-x-4 items-center text-sm font-semibold text-link rounded px-4 hover:text-white";
     const activeStyle = "bg-active text-white";
-  return (
+    return (
     <nav className="px-2 mt-4">
             <ul className="flex flex-col">
                 <li>
@@ -33,9 +33,25 @@ const Aside = () => {
                         My Library
                     </NavLink>
                 </li>
+                <li>
+            <NavLink to={'#'} className='py-2 px-6 flex items-center group text-sm text-link font-semibold hover:text-white'>
+              <span className='w-6 h-6 flex items-center justify-center mr-4 bg-white bg-opacity-60 text-black rounded-sm group-hover:bg-opacity-100'>
+                <Icon name="plus" size={12} />
+              </span>
+              Created Playlist
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={'#'} className='py-2 px-6 flex items-center group text-sm text-link font-semibold hover:text-white'>
+              <span className='w-6 h-6 flex items-center justify-center mr-4 bg-gradient-to-br from-purple-700 text-white to-blue-300 rounded-sm opacity-60 group-hover:opacity-100'>
+                <Icon name="heartFilled" size={12} />
+              </span>
+              Liked Songs
+            </NavLink>
+          </li>
             </ul>
         </nav>
-  )
+    )
 }
 
 export default Aside;
