44b01eb0ac0f9ee0621b1397f4b06aeb51bc83d1
reduansj <reduansenoun@gmail.com>
2022-10-11 12:40:13 +0200

    feat(register container): remove register container from components

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import RegisterSection from '../../components/registerSection/RegisterSection'
+import React from "react";
+
 import loginImage from "assets/register.png";
 import Logo from "components/logo";
-import RegisterForm from "../../pages/register/registerForm/RegisterForm"
+import RegisterForm from "pages/register";
 import { Link } from "react-router-dom";
+import FormSection from "components/formSection";
 
 const Register = () => {
-
   return (
-    <RegisterSection imgUrl={loginImage}>
-        <Logo width={"w-28"} />
+    <FormSection imgUrl={loginImage}>
+      <Logo width={"w-28"} />
       <div className=" text-center mb-10">
-        <h1 className="font-bold text-xl ">Your music, your <span className='text-primary'>Astro Sounds!</span></h1>
+        <h1 className="font-bold text-xl ">
+          Your music, your <span className="text-primary">Astro Sounds!</span>
+        </h1>
         <p>See you on the other side!</p>
       </div>
       <RegisterForm />
@@ -21,8 +23,8 @@ const Register = () => {
           Sign up
         </Link>
       </div>
-    </RegisterSection>
-  )
-}
+    </FormSection>
+  );
+};
 
-export default Register
+export default Register;
