1cb1753dcb7547f032a5285ac4fcfc252d4678ff
MatiasPatrignani <m.patrignani56@gmail.com>
2022-10-11 11:04:40 +0200

    Aside - Frontend

--- client/src/router/Router.js
+++ client/src/router/Router.js
@@ -4,12 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import FullScreenModal from "components/fullScreenModal";
 import Login from "pages/login";
 import Register from "../pages/register/Register";
+import Aside from "../pages/layout/aside";
 
 const Router = () => {
   return (
     <Routes>
       <Route index element={<Login />} />
       <Route path="register" element={<Register/>} />
+      <Route path="aside" element={<Aside/>} />
     </Routes>
   );
 };
