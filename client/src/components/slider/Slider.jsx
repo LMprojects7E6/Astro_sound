bb3fdcf1049959a4181d0aa42b719b7f683ee2c8
Averoli <afanasenkavolha@gmail.com>
2022-10-17 12:19:06 +0200

    update controller

--- client/src/components/slider/Slider.jsx
+++ client/src/components/slider/Slider.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Slider = ({ threeSongs }) => {
   console.log(threeSongs);
+  const artist = threeSongs[0].artist
   return (
     <section className="min-w-screen m-4 rounded-md h-72 bg-gradient-to-b from-purpleDark to-black">
       <div></div>
-      {/* <h1>{threeSongs[0].artist}</h1> */}
+      <h1>{artist}</h1>
     </section>
   );
 };
