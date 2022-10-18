15156ba7d5577d0311121cf59b786a71c6bebb49
Averoli <afanasenkavolha@gmail.com>
2022-10-18 16:56:41 +0200

    feat(Added bootstrap)

--- client/src/components/slider/Slider.jsx
+++ client/src/components/slider/Slider.jsx
@@ -1,39 +1,36 @@
 import React from "react";
-
+import "bootstrap/js/dist/carousel";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "components/button/Button";
 
 const Slider = ({ threeSongs }) => {
   return (
-    <section className="flex pt-5 min-w-screen m-4 rounded-md h-72 bg-gradient-to-b from-purpleDark to-black">
-      {threeSongs.map((song) => {
-        let { _id, artist, title, songImage } = song;
-        console.log(songImage);
-        return (
-          
-          <div>
-            <h1 className="p-5 text-white text-4xl font-bold">{artist}</h1>
-            <p className="pl-5 text-grey">
-              We recommend you listen <span>{title}</span>
-            </p>
-            <div className="pl-5 pt-20">
-              <Button
-                type={"button"}
-                text={"Play"}
-                radius={"rounded-md"}
-                // onClick={onLogout}
-                background={"bg-purple"}
-              />
-            </div>
-
-            {/* <img
-                  src={song.songImage}
-                  alt={song.artist}
-                  className="bg-cover"
-                /> */}
-          </div>
-        );
-      })}
-    </section>
+<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+  <ol className="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img className="d-block w-100" src="..." alt="First slide"/>
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src="..." alt="Second slide"/>
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src="..." alt="Third slide"/>
+    </div>
+  </div>
+  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="sr-only">Previous</span>
+  </a>
+  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="sr-only">Next</span>
+  </a>
+</div>
   );
 };
 
