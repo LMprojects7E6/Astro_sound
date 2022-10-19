5650143518529e8a608307d36ecadf8dc218225d
Ivan Escribano <ivanescribano1998@gmail.com>
2022-10-19 18:39:25 +0200

    feat(liked songs add some extra styles)

--- client/src/components/favoriteSongContainer/FavoriteSongContainer.jsx
+++ client/src/components/favoriteSongContainer/FavoriteSongContainer.jsx
@@ -12,11 +12,11 @@ const FavoriteSongContainer = ({ likedSongs }) => {
         </span>
       </h2>
       <section className="w-full mainButtonBg flex text-white rounded-lg mb-20 ">
-        <div className="p-5 w-full flex flex-col justify-around">
+        <div className="p-5 w-full flex flex-col justify-around ml-5">
           <div className="overflow-y-auto h-52">
-            <h2 className="text-xl font-bold mb-5 md:flex hidden">
+            {/* <h2 className="text-xl font-bold mb-5 md:flex hidden">
               Songs that come from your heart❤️:
-            </h2>
+            </h2> */}
             {likedSongs.map((song) => {
               return (
                 <LikedSong
@@ -27,8 +27,9 @@ const FavoriteSongContainer = ({ likedSongs }) => {
               );
             })}
           </div>
-          <div className="flex justify-between">
-            <p className="font-semibold text-2xl">
+          <div className="flex justify-between flex-col">
+            <h2 className="font-semibold text-3xl mb-4">Liked songs</h2>
+            <p className="font-normal text-xl">
               {likedSongs.length} Liked songs
             </p>
             <div className=" rounded-full border-2 border-white md:hidden pl-0.5 pt-0.5 w-8 h-8  ">
