5c537e8b66780f84f8eb07fb28b6377cff9f3a55
MatiasPatrignani <m.patrignani56@gmail.com>
2022-10-19 15:45:04 +0200

    feat(responsive-laaout

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -1,10 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 import { postSong } from "api/songs";
+import DashboardSection from "components/dashboardSection";
+import Dropdown from "components/dropdown";
 import Loader from "components/loader/Loader";
 
 import { AuthContext } from "context/AuthProvider";
+import Aside from "pages/layout/aside";
+import MusicPlayer from "pages/layout/musicPlayer";
 import { useContext } from "react";
 import { toast } from "react-hot-toast";
+import { Outlet } from "react-router-dom";
 
 const Admin = () => {
   const { user } = useContext(AuthContext);
@@ -33,8 +38,7 @@ const Admin = () => {
           <Loader />
         </div>
       )}
-
-      <div className="w-full h-screen bg-purple flex justify-center items-center">
+      {/* <div className="w-full h-screen bg-purple flex justify-center items-center">
         <form onSubmit={handleSubmit}>
           <div className="bg-grey flex flex-col p-10">
             Title :
@@ -52,7 +56,29 @@ const Admin = () => {
             <button className="mt-10 bg-black text-white">SUBMIT</button>
           </div>
         </form>
-      </div>
+      </div> */}
+
+      <DashboardSection>
+      <div className="w-full h-screen flex justify-center items-center">
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col p-10">
+            Title :
+            <input type="text" name="title" />
+            Artist :
+            <input type="text" name="artist" />
+            Album :
+            <input type="text" name="album" />
+            Genre:
+            <input type="text" name="genre" />
+            Song Image:
+            <input type="file" name="songImage" />
+            Song :
+            <input type="file" name="songFile" />
+            <button className="mt-10 bg-black text-white">SUBMIT</button>
+          </div>
+        </form>
+      </div> 
+      </DashboardSection>
     </>
   );
 };
