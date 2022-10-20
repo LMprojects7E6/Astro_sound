a22d6e463192339ebd3a4a3af54b2e7f930898ab
MatiasPatrignani <m.patrignani56@gmail.com>
2022-10-20 17:54:54 +0200

    feat(admin-style)

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -1,16 +1,17 @@
+
 import { useMutation } from "@tanstack/react-query";
 import { postSong } from "api/songs";
 import DashboardSection from "components/dashboardSection";
 import Dropdown from "components/dropdown";
 import Icons from "components/icons";
 import Loader from "components/loader/Loader";
-
 import { AuthContext } from "context/AuthProvider";
 import MusicPlayer from "pages/layout/musicPlayer";
 import { useContext } from "react";
 import { toast } from "react-hot-toast";
 import AsideAdmin from "./asideAdmin";
 import FormAdmin from "./formAdmin";
+
 
 const Admin = () => {
   const { user } = useContext(AuthContext);
@@ -25,11 +26,11 @@ const Admin = () => {
     },
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    mutate(data);
-  };
+  // const uploadSong = (e) => {
+  //   e.preventDefault();
+  //   const data = new FormData(e.target);
+  //   mutate(data);
+  // };
  
   // if (isLoading) {
   //   return <Loader></Loader>;
@@ -45,7 +46,7 @@ const Admin = () => {
       <div className="flex flex-col-reverse h-screen">
         <Dropdown admin={true}/>
         <AsideAdmin />
-        <FormAdmin handleSubmit={handleSubmit}/>
+        <FormAdmin mutate={mutate}/>
         <MusicPlayer />
       </div>
       </DashboardSection>
