34722103da122192a86cdc617ecf7319200a06b6
Averoli <afanasenkavolha@gmail.com>
2022-10-21 15:18:27 +0200

    feat(Dashboard with Dropdown)

--- client/src/router/Router.js
+++ client/src/router/Router.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "pages/layout";
 import Login from "pages/login";
 import Home from "pages/home";
-import Library from "pages/library";
+import Playlists from "pages/playlists";
 import Search from "pages/search";
 import Profile from "pages/profile";
 import Register from "pages/register";
@@ -17,7 +17,7 @@ const Router = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="library" element={<Library />} />
+          <Route path="playlist" element={<Playlists />} />
           <Route path="likedSongs" element={<LikedSongs />} />
           <Route path="search" element={<Search />} />
           <Route path="profile" element={<Profile />} />
