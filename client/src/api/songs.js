33a7d9010551c02447970e603d9c154dcedc2178
MiguelD-PC <mikdominguez@gmail.com>
2022-10-24 11:19:53 +0100

    changes to api

--- client/src/api/songs.js
+++ client/src/api/songs.js
@@ -5,37 +5,44 @@ export const postSong = async (data) => {
   const response = await api.post("/songs", data);
   return response.data;
 };
+
 //!GET ALL SONGS
 export const getSongs = async () => {
   const response = await api.get("/songs");
   return response.data;
 };
+
 //!GET THREE SONGS
 export const getThreeSongs = async () => {
   const response = await api.get("/songs/threeSongs");
   return response.data;
 };
+
 //!GET SONGS BY GENRE
 export const getSongsByGenre = async (genre) => {
   const response = await api.get(`/songs/${genre}`);
   return response.data;
 };
+
 //!GET ALL SONGS FROM A PLAYLIST
 export const getAllSongsFromPlaylist = async (playlistID) => {
   const response = await api.get(`/songs/playlist/${playlistID}`);
   return response.data;
 };
+
 //!POST UPDATE PLAYLIST WITH A NEW SONG
 export const addSongToPlaylist = async (data) => {
   const response = await api.post(`/songs/${data.songId}/${data.playlistId}`);
   return response.data;
 };
+
 //!POST UPDATE LIKED PLAYLIST WITH A NEW SONG
 export const addSongToLikedPlaylist = async (songID) => {
   const response = await api.post(`/songs/${songID}`);
   return response.data;
 };
 
+//!DELETE SONG FROM LIKED PLAYLIST
 export const removeSongFromLikedPlaylist = async (songID) => {
   const response = await api.delete(`/songs/${songID}`);
   return response.data;
