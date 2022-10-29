600aa2b46cb347a164c8717d236f88e54549a74c
Ivan Escribano <ivanescribano1998@gmail.com>
2022-10-29 21:32:17 +0200

    feat(play song on play in search and playlist)

--- client/src/context/MusicPlayerProvider.jsx
+++ client/src/context/MusicPlayerProvider.jsx
@@ -2,43 +2,6 @@ import { createContext, useReducer, useState } from "react";
 import musicPlayerReducer from "store/reducers/musicPlayerReducer";
 
 export const MusicPlayerContext = createContext();
-//!!!! TEST BELOW
-const testData = [
-  {
-    id: "6357cf2066c6bb63e7e08f1b",
-    songFile:
-      "https://res.cloudinary.com/dfmlnoenk/video/upload/v1666699040/songFiles/qaxeygwyvhompoma97fc.mp3",
-    title: "Without me",
-    artist: "Eminem",
-    album: "8 mile",
-    songImage:
-      "https://res.cloudinary.com/dfmlnoenk/image/upload/v1666699039/songImages/jwblxulpncsmbqfiyvde.jpg",
-    genre: "rap",
-  },
-  {
-    id: "6357cf4a66c6bb63e7e08f1f",
-    songFile:
-      "https://res.cloudinary.com/dfmlnoenk/video/upload/v1666699082/songFiles/buknyc5t4exv6ittddn4.mp3",
-    title: "Wanksta",
-    artist: "50 cent",
-    album: "get rich or try dying",
-    songImage:
-      "https://res.cloudinary.com/dfmlnoenk/image/upload/v1666699081/songImages/h8zmox9h1jiywxmocb0m.jpg",
-    genre: "rap",
-  },
-  {
-    id: "6357cf9066c6bb63e7e08f23",
-    songFile:
-      "https://res.cloudinary.com/dfmlnoenk/video/upload/v1666699151/songFiles/n8du4x3npx4i4vfs0oob.mp3",
-    title: "Picture me rollin",
-    artist: "2Pac",
-    album: "All eyez on me",
-    songImage:
-      "https://res.cloudinary.com/dfmlnoenk/image/upload/v1666699150/songImages/mgcec6lqukyhbtmu1sho.jpg",
-    genre: "rap",
-  },
-];
-//!!!! TEST ABOVE
 const MusicPlayerProvider = ({ children }) => {
   const initialData = {
     playing: false,
@@ -48,7 +11,7 @@ const MusicPlayerProvider = ({ children }) => {
     muted: false,
     songNumber: 0,
   };
-  const [musicPlayer, setMusicPlayer] = useState(testData);
+  const [musicPlayer, setMusicPlayer] = useState([]);
   const [controls, dispatch] = useReducer(musicPlayerReducer, initialData);
   const value = {
     musicPlayer,
