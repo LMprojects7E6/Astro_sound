7e67d8e48a32eebf53ecff4c9179cf6dbc2c08c5
Ivan Escribano <ivanescribano1998@gmail.com>
2022-10-31 16:23:22 +0100

    feat(added process env in api)

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 });
 
